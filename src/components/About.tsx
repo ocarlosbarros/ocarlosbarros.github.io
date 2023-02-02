68af27bd2d18b7490a8d7ff2caf72aa6ad9b1f0b
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-02 16:22:55 -0300

    feat(About):creates component for about section

---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "./Heading";
+
+export function About() {
+    return (
+        <section className="text-black bg-gradient-to-b from-['#BFB6E0'] to-[#BFB6E0]">
+            <Heading text="Bem vindo(a)"/>
+        </section>
+    )
+}
