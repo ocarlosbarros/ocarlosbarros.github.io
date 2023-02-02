174f7dee3a4ecec7390f71f44260f0948f627eb6
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-02 16:24:24 -0300

    feat(App):uses the new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Header } from "./components/Header"
+import { About } from "./components/About";
+import { Header } from "./components/Header";
+import { Projects } from "./components/Projects";
+
 
 function App() {
   return (
     <main>
-        <Header />
+          <Header />
+          <About />
+          <Projects />
     </main>
   )
 }
