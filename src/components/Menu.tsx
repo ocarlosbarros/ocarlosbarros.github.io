63e1bba39c1ae66e4cd849f49b64d4bc0ef25f83
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-02 12:41:27 -0300

    feat(Menu):creates Menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,18 @@
+export function Menu() {
+    return (
+        <nav className="w-screen h-[15%] flex grow items-center justify-between pl-10">
+            <div>
+                <p className="text-3xl">&lt; C B / &gt;</p>
+            </div>
+            <ul className="text-xl w-1/2 flex justify-around items-center">
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+            </ul>
+            <div>
+
+            </div>
+        </nav>
+    )
+}
