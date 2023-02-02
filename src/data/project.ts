369e4db1652e0e48f3dbe5822a527137632a0b39
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-02 16:21:36 -0300

    feat:creates data example for projects cards

---
+++ src/data/project.ts
@@ -0,0 +1,44 @@
+export default [
+    {
+        id: 1,
+        title: 'delivery-app',
+        image: 'http://',
+        description: 'Full Stack Application built using React, Styled Components, Nodejs, Express, Sequelize, and MySQL. The Application is a beverage delivery with features for users, sellers and admins using JWT auth…',
+        labels: ['React', 'Styled Components', 'Node.js', 'Express', 'Sequelize', 'MySQL', 'JWT']
+    },
+    {
+        id: 2,
+        title: 'esports-app',
+        image: 'http://',
+        description: 'NLW eSports App is an application Full Stack with an API REST created with Node.js and Typescript in Backend, Frontend React to Web and mobile version using React Native.',
+        labels: ['Node.js', 'Typescript', 'React', 'React-Native']
+    },
+    {
+        id: 3,
+        title: 'trybewallet',
+        image: 'http://',
+        description: 'Aplicação React utilizando Redux, Redux-Thunk com consulta Awesomeapi API para as cotações das moedas. Esta aplicação tem como objetivo cadastrar e exibir despesas e seu valor total convertido para…',
+        labels: ['React', 'Redux', 'Redux-Thunk']
+    },
+    {
+        id: 4,
+        title: 'store-manager',
+        image: 'http://',
+        description: 'API RESTFUL Nodejs using Model, Service, Controller architecture, MySQL database to manage simple CRUD to sales and products applying TDD with unit tests.',
+        labels: ['Node.js', 'MySQL']
+    },
+    {
+        id: 5,
+        title: 'nlw-valoriza',
+        image: 'http://',
+        description: 'API NLW Valoriza criada durante a Next Level Week 06 - Together - com Daniele Leão - Rocketseat',
+        labels: ['Node.js']
+    },
+    {
+        id: 6,
+        title: 'trivia-game',
+        image: 'http://',
+        description: 'This app was built in React. Trivia Game is a competition where users compete by answering interesting questions and each correct answer earns points.',
+        labels: ['React', 'Node.js']
+    }
+]
