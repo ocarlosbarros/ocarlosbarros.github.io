b0edc24d7e4a370dc322ff940227e555b38f6893
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-03 18:04:02 -0300

    refactor(Header):refactor css rules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,17 +5,17 @@ import { MouseSimple, CaretDoubleDown } from 'phosphor-react';
 export function Header() {
     return (
 
-        <header className="flex-col bg-gradient-to-b from-primary to-secondary w-screen">
-            <div className="h-screen w-screen bg-no-repeat bg-cover bg-waves border-primary">
+        <header className="flex-col bg-gradient-to-b from-primary to-secondary">
+            <div className="h-screen bg-no-repeat bg-cover bg-waves border-primary">
             <Menu />
             <div className="flex flex-wrap justify-center items-end w-full h-[80%]">
                 <h1 className="text-6xl">Olá, eu sou o <span className="text-highlight">Carlos Barros</span></h1>
                 <div className="flex w-full justify-center items-center">
-                    <a href="#projects" className="flex-column relative top-5 right-3 cursor-pointer">
+                    <a href="#about" className="flex-column relative top-5 right-3 cursor-pointer">
                         <MouseSimple className="rotate-180 relative left-1" size={30} />
                         <CaretDoubleDown className="animate-bounce relative top-2" size={38} />
                     </a>
-                    <a href="#projects" className="cursor-pointer"><p className="text-2xl">Visualizar projetos</p></a>
+                    <a href="#about" className="cursor-pointer"><p className="text-2xl">Saiba mais</p></a>
                 </div>
                 </div>
             </div>
