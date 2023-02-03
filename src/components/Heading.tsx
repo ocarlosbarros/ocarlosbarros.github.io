08f299a391ca475412de8424313015222adf851b
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-03 17:55:51 -0300

    refactor:change css rule

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,5 +3,5 @@ interface HeadingProps {
 }
 
 export function Heading({ text } : HeadingProps) {
-    return <h2 id="projects" className="m-20 text-4xl">{text}</h2>
+    return <h2 id="projects" className="m-10 text-4xl">{text}</h2>
 }
