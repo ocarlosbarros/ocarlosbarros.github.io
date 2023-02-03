1ce2b3a6c05eb95843b051a20983662898d579c4
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-02 21:23:57 -0300

    refactor:changes key value in map

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,13 +6,12 @@ import { CardProps } from './Card';
 import projects from '../data/project';
 
 export function Projects() {
-    console.log(projects);
     return (
         <section className="text-black">
             <Heading text="Projetos" />
             {
-                projects.map(({title, image, description, labels }: CardProps) => {
-                    return <Card key={title} title={title} image={image} labels={ labels } description={description} />
+                projects.map(({id, title, image, description, labels }: CardProps) => {
+                    return <Card key={id} title={title} image={image} labels={ labels } description={description} />
                 })
 
 
