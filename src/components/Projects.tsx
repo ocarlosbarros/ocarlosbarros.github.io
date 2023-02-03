b1f7e9d0140c222c4ecefedcbc1722282b0045a3
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-03 18:04:32 -0300

    refactor(Projects):refactor css rules

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "./Heading";
+import { Content } from "./Content";
 
 import { Card } from "./Card";
 import { CardProps } from './Card';
@@ -7,8 +8,10 @@ import projects from '../data/project';
 
 export function Projects() {
     return (
-        <section className="text-black">
-            <Heading text="Projetos" />
+        <div className="text-black w-full h-screen">
+        <Heading id="projects" text="Projetos" />
+        <Content className="text-black w-full h-screen p-5 flex" >
+            <div className="flex flex-wrap items-center justify-center p-2">
             {
                 projects.map(({id, title, image, description, labels }: CardProps) => {
                     return <Card key={id} title={title} image={image} labels={ labels } description={description} />
@@ -16,7 +19,8 @@ export function Projects() {
 
 
             }
-
-        </section>
+            </div>
+            </Content>
+        </div>
     )
 }
