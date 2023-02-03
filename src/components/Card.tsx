3046de569ad5da4642fcf9771fcecce3c75dd5da
Carlos Barros <carlos_dbs@hotmail.com>
2023-02-03 18:05:33 -0300

    refacto(Card)r:remove labels in props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,15 +6,12 @@ export interface CardProps {
     description: string;
 
 }
-export function Card({ title, image, labels, description }: CardProps) {
+export function Card({ title, image, description }: CardProps) {
     return (
-        <article className="w-[426px] h-[550px] border" >
+        <article className="bg-white shadow-gray-400 shadow-lg w-[426px] h-[550px] flex-col items-center m-4 p-8" >
             <img src={image} alt="" />
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <div>
-                {labels.map((label) => <span>{ label }</span>)}
-            </div>
+            <h3 className="text-2xl mt-2 mb-2">{title}</h3>
+            <p className="m2">{description}</p>
         </article>
     );
 }
