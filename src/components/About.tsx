94eebcb9538c6ab1b0ba799b7aa38fb05f703fcb
Carlos Barros <carlos_dbs@hotmail.com>
2023-04-06 19:54:45 -0300

    fix(About): fix css values

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,33 +8,37 @@ import profileImage from '../assets/profile.png';
 export function About() {
     return (
         <Content className="text-black w-full h-screen p-5 flex">
-            <div className="w-1/2 h-screen">
-                <div className="h-[91.2%] flex items-center justify-center">
-                   <div className="text-center h-[50%] p-5 text-lg">
-                        <Heading id="about" text="Olá" />
-                        <p className="p-1">
-                            Olá, sou Carlos Barros. Iniciei minha trajetório em tecnologia em 2015
-                            através do curso de Sistemas para Internet pela FATEC-Carapicuíba. Formado
-                            em Desenvolvimento Web pela Trybe, apaixonado por tecnologia, por aprender coisas novas e desafios. Busco com a tecnologia trazer impacto positivo para as pessoas e para sociedade.
-                        </p>
-                        <p className="p-1">
-                            Olá, sou Carlos Barros. Iniciei minha trajetório em tecnologia em 2015
-                            através do curso de Sistemas para Internet pela FATEC-Carapicuíba. Formado
-                            em Desenvolvimento Web pela Trybe, apaixonado por tecnologia, por aprender coisas novas e desafios. Busco com a tecnologia trazer impacto positivo para as pessoas e para sociedade.
-                        </p>
-                        <p className="p-1">Criando conteúdo em vídeo no Youtube</p>
+            <div className="w-screen h-screen flex">
+                <div className="w-1/2 h-screen flex items-center justify-center">
+                   <div className="text-center p-5 text-lg">
+                        <Heading id="about" text="Olá!" />
+                        <div className="text-center">
+                            <p className="p-1">
+                                Olá, sou Carlos Barros. Sou apaixonado por tecnologia, por aprender coisas novas e por desafios.
+                                Encontrei na área de tecnologia uma forma de manter esses desejos e trazer impacto positivo para a vida das pessoas
+                                sociedade.
+                            </p>
+                            <p className="p-1">
+                                Iniciei minha trajetória em desenvolvimento de software em 2015 através do curso de Sistemas para Internet pela FATEC
+                                ,onde cursei 4 semestres, mas sou formado no curso de Desenvolvimento Web pela Trybe.
+                            </p>
+                            <p className="p-1">
+                                Atualmente buscando oportunidades para impulsionar minha carreira como desenvolvedor
+                                front-end, almejando uma carreira de mais técnica e aspirante a criação de conteúdo tech.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="w-1/2 h-[100%] flex flex-col justify-center items-center ">
-                <div className="flex-col justify-center items-center h-1/2 m-4 align-middle">
-                <img className="m-auto h-[350px]" src={profileImage} alt="" />
-                    <p className="text-center mt-8 text-4xl" >Carlos Barros</p>
-                </div>
-                <div className="flex justify-center">
-                    <a href="https://github.com/ocarlosbarros" target="_blank"><GithubLogo size={62} /></a>
-                    <a href="https://www.linkedin.com/in/ocarlosbarros/" target="_blank"><LinkedinLogo size={64} /></a>
-                    <a href="https://www.instagram.com/o_carlosbarros/" target="_blank"><InstagramLogo size={62} /></a>
+                <div className="w-1/2 h-100 flex  flex-col items-center">
+                    <div className="m-auto">
+                        <img className="w-[250px] h-[250px]" src={profileImage} alt="" />
+                        <div className="flex justify-center m-4">
+                            <a href="https://github.com/ocarlosbarros" target="_blank"><GithubLogo size={50} /></a>
+                            <a href="https://www.linkedin.com/in/ocarlosbarros/" target="_blank"><LinkedinLogo size={50} /></a>
+                            <a href="https://www.instagram.com/o_carlosbarros/" target="_blank"><InstagramLogo size={50} /></a>
+                        </div>
+                        <p className="text-center text-3xl" >Carlos Barros</p>
+                    </div>
                 </div>
             </div>
         </Content>
