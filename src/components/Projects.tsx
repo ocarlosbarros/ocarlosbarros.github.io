b853831514800e38993128b69943525b04bed425
Carlos Barros <carlos_dbs@hotmail.com>
2023-04-06 19:54:54 -0300

    fix(Project): fix css values

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,19 +8,19 @@ import projects from '../data/project';
 
 export function Projects() {
     return (
-        <div className="text-black w-full h-screen">
-        <Heading id="projects" text="Projetos" />
-        <Content className="text-black w-full h-screen p-5 flex" >
-            <div className="flex flex-wrap items-center justify-center p-2">
-            {
-                projects.map(({id, title, image, description, labels }: CardProps) => {
-                    return <Card key={id} title={title} image={image} labels={ labels } description={description} />
-                })
+        <Content className="text-black w-full h-screen p-5 flex">
+            <div className="text-center p-5 text-lg">
+                <Heading id="projects" text="Projetos" />
+                <div className="flex flex-wrap items-center justify-center p-2">
+                {
+                    projects.map(({id, title, image, description, labels }: CardProps) => {
+                        return <Card key={id} title={title} image={image} labels={ labels } description={description} />
+                    })
 
 
-            }
+                }
+                </div>
             </div>
             </Content>
-        </div>
     )
 }
